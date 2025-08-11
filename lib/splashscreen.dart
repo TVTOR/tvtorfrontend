dd27f56f2b5eec81a74887fe61a22f88c6026f93
Behzad <behzad@poxito.com>
2025-08-11 19:33:46 +0500

    first commit

--- lib/splashscreen.dart
+++ lib/splashscreen.dart
@@ -13,7 +13,7 @@ class SplashScreen extends StatefulWidget {
 
 class _SplashScreenState extends State<SplashScreen> {
   @override
-  Future<void> initState() async{
+  void initState() {
     super.initState();
 
     if (mounted) {
